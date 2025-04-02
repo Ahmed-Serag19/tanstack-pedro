512c327513e6f17dfc45b6b6d1668096354b469e
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2025-04-02 17:17:59 +0200

    finished first basic example to showcase fetch posts using react tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
-
 const url = "https://jsonplaceholder.typicode.com/posts";
 
+const fetchPosts = async () => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Error fetching posts");
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>Error occured: {error.message}</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
     </>
   );
 }
